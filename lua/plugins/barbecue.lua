a1736e1cf0ccdaaa68ab2ccb75ce4f9e593d75e2
Ether <134476332+etherTide@users.noreply.github.com>
2026-05-24 22:31:33 +1000

    Added barbecue
    vscode style breadcrumbs, didn't even remember that vscode feature so
    thx to folke suggesting this plugin in the TokyoNight README

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+{
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  opts = {
+    theme = 'tokyonight',
+  },
+}
