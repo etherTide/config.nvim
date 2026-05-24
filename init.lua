ad91605502ea0239a16ec2d81fb4abd7cf327c0f
Ether <134476332+etherTide@users.noreply.github.com>
2026-05-24 22:27:31 +1000

    Added lazy.nvim as a plugin manager
    set TokyoNight as the colourscheme but isn't added yet

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- Leader keys need to be mapped b4 lazy.nvim is loaded.
+-- They are assigned inside config.lazy but if I also do it here I won't forget
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require "config.lazy"
+
